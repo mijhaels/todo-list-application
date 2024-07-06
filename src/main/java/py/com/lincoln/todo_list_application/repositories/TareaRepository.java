bf663a607f4e09c8fd8770e3ccfe79c11c8d7060
Mijhael <mijhael.samaniego@konecta.com.py>
2024-07-06 18:45:20 -0400

    refactor: Eliminar código no utilizado en TareaRepository

--- src/main/java/py/com/lincoln/todo_list_application/repositories/TareaRepository.java
+++ src/main/java/py/com/lincoln/todo_list_application/repositories/TareaRepository.java
@@ -2,12 +2,6 @@ package py.com.lincoln.todo_list_application.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import py.com.lincoln.todo_list_application.models.Tarea;
-import py.com.lincoln.todo_list_application.models.Usuario;
-
-import java.util.List;
 
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
-
-    List<Tarea> findAllByUsuario(Usuario usuario);
-
 }
