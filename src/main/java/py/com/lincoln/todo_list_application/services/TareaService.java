5c5b7741170942eda8f28de7468dab3ecfc5a4c6
Mijhael <mijhael.samaniego@konecta.com.py>
2024-07-06 18:43:22 -0400

    feat: Actualizar TareaService para usar TareaInputDto y TareaEditDto

--- src/main/java/py/com/lincoln/todo_list_application/services/TareaService.java
+++ src/main/java/py/com/lincoln/todo_list_application/services/TareaService.java
@@ -1,42 +1,58 @@
 package py.com.lincoln.todo_list_application.services;
 
 import org.springframework.stereotype.Service;
+import py.com.lincoln.todo_list_application.dtos.TareaDto;
+import py.com.lincoln.todo_list_application.dtos.TareaEditDto;
+import py.com.lincoln.todo_list_application.dtos.TareaInputDto;
+import py.com.lincoln.todo_list_application.mappers.Mapper;
 import py.com.lincoln.todo_list_application.models.Tarea;
 import py.com.lincoln.todo_list_application.models.Usuario;
 import py.com.lincoln.todo_list_application.repositories.TareaRepository;
+import py.com.lincoln.todo_list_application.repositories.UsuarioRepository;
 
 import java.util.List;
 
 @Service
 public class TareaService {
 
-    private TareaRepository tareaRepository;
+    private final TareaRepository tareaRepository;
 
-    public TareaService(TareaRepository tareaRepository) {
+    private final UsuarioRepository usuarioRepository;
+
+    private final Mapper mapper;
+
+    public TareaService(TareaRepository tareaRepository, UsuarioRepository usuarioRepository, Mapper mapper) {
         this.tareaRepository = tareaRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.mapper = mapper;
     }
 
-    public Tarea guardarTarea(Tarea tarea) {
+    public Tarea crearTarea(TareaInputDto input) {
+        Usuario usuario = usuarioRepository.findById(input.getUsuarioId()).orElseThrow(() -> new RuntimeException("Usuario no encontrado."));
+        Tarea tarea = new Tarea();
+        tarea.setTitulo(input.getTitulo());
+        tarea.setDescripcion(input.getDescripcion());
+        tarea.setCompletado(false);
+        tarea.setUsuario(usuario);
         return tareaRepository.save(tarea);
     }
 
-    public List<Tarea> obtenerTareas() {
-        return tareaRepository.findAll();
+    public List<TareaDto> obtenerTareas() {
+        return tareaRepository.findAll().stream().map(mapper::toDto).toList();
     }
 
-    public Tarea obtenerTareaPorId(Long id) {
-        return tareaRepository.findById(id).orElse(null);
-    }
-
-    public List<Tarea> obtenerTareasPorUsuario(Usuario usuario) {
-        return tareaRepository.findAllByUsuario(usuario);
-    }
-
-    public Tarea actualizarTarea(Tarea tarea) {
+    public Tarea actualizarTarea(Long idTarea, TareaEditDto input) {
+        Usuario usuario = usuarioRepository.findById(input.getUsuarioId()).orElseThrow(() -> new RuntimeException("Usuario no encontrado."));
+        Tarea tarea = tareaRepository.findById(idTarea).orElseThrow(() -> new RuntimeException("Tarea no encontrada."));
+        tarea.setTitulo(input.getTitulo());
+        tarea.setDescripcion(input.getDescripcion());
+        tarea.setCompletado(input.getCompletado());
+        tarea.setUsuario(usuario);
         return tareaRepository.save(tarea);
     }
 
     public String eliminarTarea(Long id) {
+        tareaRepository.findById(id).orElseThrow(() -> new RuntimeException("Tarea no encontrada."));
         tareaRepository.deleteById(id);
         return "Tarea eliminada con éxito.";
     }
